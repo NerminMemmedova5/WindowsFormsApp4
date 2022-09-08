61fa149d26b1526ecc39fb9dbd64c813750ff18e
Nermin Memmedova5 <nerminmemmedova379@gmail.com>
2022-09-08 03:39:05 +0100

    Add project files.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.londonbtn = new System.Windows.Forms.Button();
+            this.bakubtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(226, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 79);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Word Clocks";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // londonbtn
+            // 
+            this.londonbtn.BackColor = System.Drawing.Color.Fuchsia;
+            this.londonbtn.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.londonbtn.ForeColor = System.Drawing.Color.Yellow;
+            this.londonbtn.Location = new System.Drawing.Point(115, 163);
+            this.londonbtn.Name = "londonbtn";
+            this.londonbtn.Size = new System.Drawing.Size(201, 93);
+            this.londonbtn.TabIndex = 1;
+            this.londonbtn.Text = "London";
+            this.londonbtn.UseVisualStyleBackColor = false;
+            this.londonbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bakubtn
+            // 
+            this.bakubtn.BackColor = System.Drawing.Color.Fuchsia;
+            this.bakubtn.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bakubtn.ForeColor = System.Drawing.Color.Yellow;
+            this.bakubtn.Location = new System.Drawing.Point(491, 163);
+            this.bakubtn.Name = "bakubtn";
+            this.bakubtn.Size = new System.Drawing.Size(201, 93);
+            this.bakubtn.TabIndex = 2;
+            this.bakubtn.Text = "Baku";
+            this.bakubtn.UseVisualStyleBackColor = false;
+            this.bakubtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bakubtn);
+            this.Controls.Add(this.londonbtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button londonbtn;
+        private System.Windows.Forms.Button bakubtn;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(sender is Button bt)
+            {
+               bt.Dispose();
+            }
+            this.label1.Dispose();
+            this.bakubtn.Dispose();
+            this.BackgroundImage = Properties.Resources.London;
+            
+
+            Label londonlb = new Label();
+            londonlb.Location = new Point(320, 30);
+            londonlb.ForeColor = Color.Red;
+            londonlb.Text = DateTime.Now.AddHours(-3).ToLongTimeString();
+
+            londonlb.Font = new Font("Monotype Corsiva", 25, FontStyle.Bold);
+            londonlb.AutoSize = true;
+
+            this.Controls.Add(londonlb);
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(sender is Button bt)
+            {
+                bt.Dispose();
+            }
+            this.label1.Dispose();
+            this.londonbtn.Dispose();
+            this.BackgroundImage = Properties.Resources.Baki;
+
+            Label bakulb = new Label();
+            bakulb.Location = new Point(320, 30);
+            bakulb.ForeColor = Color.Red;
+            bakulb.Text = DateTime.Now.ToLongTimeString();
+            bakulb.Font = new Font("Monotype Corsiva", 25, FontStyle.Bold);
+            bakulb.AutoSize = true;
+
+            this.Controls.Add(bakulb);
+
+
+        }
+    }
+}
